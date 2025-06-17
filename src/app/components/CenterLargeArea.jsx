b43b30e1f713c6c366688cfbd7bea8b9f47e564b
Agriby Chaniago <agrieby.chaniago@student.uhb.ac.id>
2025-06-17 14:50:17 +0700

    wireframe lagi, tambah section, tambah layout, bg body, perbaikan path, rename file, rename folder

---
+++ src/app/components/CenterLargeArea.jsx
@@ -0,0 +1,7 @@
+export default function CenterLargeArea() {
+  return (
+    <div className='col-start-1 col-end-11 row-start-1 row-end-7 bg-red-200'>
+      {/* Konten area tengah besar */}
+    </div>
+  );
+}
---
+++ src/app/layout/About.jsx
@@ -0,0 +1,13 @@
+import CenterLargeArea from "../components/CenterLargeArea";
+import LeftLowerSection from "../components/LeftLowerSection";
+import RightLowerSection from "../components/RightLowerSection";
+
+export default function About() {
+  return (
+    <div className='grid grid-cols-10 grid-rows-9 gap-0 w-full h-screen'>
+      <CenterLargeArea />
+      <LeftLowerSection />
+      <RightLowerSection />
+    </div>
+  );
+}

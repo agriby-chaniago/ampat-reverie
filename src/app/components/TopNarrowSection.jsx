d063623a55632b4257c83638017af019470eeb1a
Fakhri428 <fakhri.zahi@uhb.ac.id>
2025-06-18 21:30:38 +0700

    TopNarrow

--- src/app/components/TopNarrowSection.jsx
+++ src/app/components/TopNarrowSection.jsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { Menu, Bell } from 'lucide-react';
+
 export default function TopNarrowSection() {
-  return <div className='w-full h-full bg-amber-600/70 p-2'>Top Narrow</div>;
+  return (
+    <div className="w-[517px] h-[82px] bg-white rounded-[20px] flex items-center justify-between px-4 shadow">
+      <Menu size={28} className="text-black" />
+      <Bell size={28} className="text-black" />
+    </div>
+  );
 }

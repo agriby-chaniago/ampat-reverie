eb9b8a8a4ac1198864f19e33b52973383d4f27cb
Agriby Chaniago <agrieby.chaniago@student.uhb.ac.id>
2025-06-20 17:55:49 +0700

    ketinggalan

--- src/app/layout/Why.jsx
+++ src/app/layout/Why.jsx
@@ -1,23 +1,23 @@
 export default function Why() {
   return (
-    <div className='grid grid-cols-2 grid-rows-3 gap-0 pt-[75px] mb-10 h-[calc(100vh-166px)] w-full bg-gray-100'>
+    <div className='grid grid-cols-2 grid-rows-3 w-full h-[calc(100vh-166px)] pt-[75px] mb-10 bg-gray-100 gap-0'>
+      {/* Header spans 2 columns */}
+      <div className='col-span-2 flex items-center justify-center bg-white border text-xl font-semibold'>
+        Why visit Raja Ampat?
+      </div>
+
+      {/* Konten */}
       <div className='flex items-center justify-center bg-white border'>
-        why visit raja ampat?
+        Konten 1
       </div>
       <div className='flex items-center justify-center bg-white border'>
-        kosong
+        Konten 2
       </div>
       <div className='flex items-center justify-center bg-white border'>
-        konten 1
+        Konten 3
       </div>
       <div className='flex items-center justify-center bg-white border'>
-        konten 2
-      </div>
-      <div className='flex items-center justify-center bg-white border'>
-        konten 3
-      </div>
-      <div className='flex items-center justify-center bg-white border'>
-        konten 4
+        Konten 4
       </div>
     </div>
   );
